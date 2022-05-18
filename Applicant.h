13b968a0296ce9c7baecf86a956aad20b618aef6
it21172878 <99156192+it21172878@users.noreply.github.com>
2022-05-18 21:28:54 +0530

    Update Applicant.h

--- Applicant.h
+++ Applicant.h
@@ -1,1 +1,20 @@
+#include <string>
+using namespace std;
+
+class Applicant
+{
+private:
+	int aID;
+	string aName;
+	int mobileNO;
+	string email;
+	string address;
+	string username;
+	string password;
+
+public:
+	Applicant();
+	int printApplicantDetails();
+	~Applicant();
+};
 
